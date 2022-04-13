6f26070bb55c8a794ac5094ee5c4c6bcb5d9f690
buhtigvainmet <103345897+SylvainMetivier@users.noreply.github.com>
2022-04-13 11:33:36 -0400

    Commit2022-04-13_11h33m00s
    Commit2022-04-13_11h33m00s

---
+++ Atelier-B-Exercice 1.1/Atelier-B-Exercice 1.1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Créez un programme C# affichant votre nom dans le terminal.
+
+Console.Write("Hello ");
+Console.WriteLine("World!");
+await Task.Delay(5000);
